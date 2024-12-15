6ebec47c4285c27bbfdca855910b0948992eae47
mustafatugrul <mustafatugrul034@gmail.com>
2024-12-15 18:10:00 +0300

    Backend update-profile and check completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,20 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
-import {connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT,()=>{
-    console.log("server is running on PORT:" + PORT);
-    connectDB();
+app.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
