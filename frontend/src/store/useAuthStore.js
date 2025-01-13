ae56747499bc43dc7239f18ae027b99c4d91ff12
mustafatugrul <mustafatugrul034@gmail.com>
2025-01-13 15:20:44 +0300

    frontend Home page - Sidebar created

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -7,8 +7,8 @@ export const useAuthStore = create((set) => ({
   isSigninUp: false,
   isLogginIng: false,
   isUpdatingProfile: false,
-
   isCheckingAuth: true,
+  onlineUsers: [],
 
   checkAuth: async () => {
     try {
